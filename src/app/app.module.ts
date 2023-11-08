ed7e004ef8b683cd7f64b491d4f257079bd263d0
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-11-08 16:37:16 +0200

    book and picture services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BookService } from './services/book.service';
+import { PictureService } from './services/picture.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,10 @@ import { ListComponent } from './components/list/list.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    PictureService,
+    BookService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
